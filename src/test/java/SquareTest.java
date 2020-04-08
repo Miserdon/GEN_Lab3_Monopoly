c8c20ed700576fb1be690f27c8f27cc86c10b825
robincuenoud <robin.cuenoud@heig-vd.ch>
2020-04-08 13:30:23 +0200

    updated test on Square

--- src/test/java/SquareTest.java
+++ src/test/java/SquareTest.java
@@ -2,9 +2,10 @@ import org.junit.jupiter.api.Test;
 
 
 class SquareTest {
-    
+
     @Test
     void squareReturnGoodName() {
-
+    Square sq = new Square("testName");
+    assert("testName".equals(sq.getName()));
     }
 }
