4b8f3bb0cabaaa5624063bd4f5cd09e0dcd8e308
Florian <florian.mulhauser@arcanite.ch>
2020-04-22 14:27:02 +0200

    finished MGame class

--- src/main/java/MGame.java
+++ src/main/java/MGame.java
@@ -18,6 +18,41 @@ public class MGame {
         }
 
         this.board = new Board();
+        this.roundCounter = 0;
+
+        // create the dice
+        this.dice = new Die[NB_DIE];
+        for (int i = 0; i < NB_DIE; i++){
+            dice[i] = new Die();
+        }
+
+        // create the players
         this.players = new Player[numberPlayers];
+        int playerID = 0;
+
+        for (int i = 0; i < numberPlayers; i++) {
+            String playerName = "Player " + playerID;
+            PlayerPieceType token = PlayerPieceType.values()[i];
+            players[i] = new Player(playerName, token, board, board.getSquare(0), dice);
+
+        }
+    }
+
+    public void round(){
+
+        // make each player play their turn
+        for (Player p:
+             players) {
+            p.takeTurn();
+        }
+    }
+
+
+    public void play(){
+        while(roundCounter < NB_ROUNDS){
+            round();
+            roundCounter++;
+        }
+
     }
 }
