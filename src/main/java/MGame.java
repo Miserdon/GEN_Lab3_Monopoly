00537743dd38cc33f83479a9177e90e128900b7d
Florian <florian.mulhauser@arcanite.ch>
2020-04-22 13:56:42 +0200

    WIP MGame class

--- src/main/java/MGame.java
+++ src/main/java/MGame.java
@@ -4,6 +4,20 @@ public class MGame {
     private static final int NB_ROUNDS = 20;
     private Board board;
     private Player[] players;
+    private Die[] dice;
+    private int roundCounter;
 
 
+    // constructor
+    public MGame(int numberPlayers){
+        //assertions
+        if (numberPlayers < 2) {
+            throw new IllegalArgumentException("Number of players should be greater than 2");
+        } else if(numberPlayers > 8 ){
+            throw new IllegalArgumentException("Number of players should be less than 8");
+        }
+
+        this.board = new Board();
+        this.players = new Player[numberPlayers];
+    }
 }
