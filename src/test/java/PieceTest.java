50ff03ecab5114a56c3487a644fe9d8150906f71
robincuenoud <48149097+robincuenoud@users.noreply.github.com>
2020-04-22 14:43:32 +0200

    fixed Piece and added PieceTest

---
+++ src/test/java/PieceTest.java
@@ -0,0 +1,41 @@
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PieceTest {
+   private Square initialPos;
+   private Piece initialPiece;
+
+    @BeforeEach
+    void setupPieceWithInitialPos() {
+         initialPos = new Square("initial");
+         initialPiece = new Piece(initialPos);
+    }
+
+    @Test
+    void constructorWorksWithPositionAndGetterWorks() {
+        assertEquals(initialPiece.getPosition(),initialPos);
+    }
+
+    @Test
+    void setPositionWorksAsExpected() {
+        Square secondPosition = new Square("second place");
+        Square thirdPosition = new Square("third place");
+        initialPiece.setPosition(secondPosition);
+        assertEquals(initialPiece.getPosition(),secondPosition);
+        initialPiece.setPosition(thirdPosition);
+        assertEquals(initialPiece.getPosition(),thirdPosition);
+    }
+
+    @Test
+    void constructorFailWithNullPositionGiven() {
+        assertThrows(IllegalArgumentException.class,() -> new Piece(null));
+    }
+
+    @Test
+    void setPositionFailWithNullArgument() {
+        assertThrows(IllegalArgumentException.class,() -> initialPiece.setPosition(null));
+    }
+}
